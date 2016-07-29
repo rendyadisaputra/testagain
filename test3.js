fee61da995050068f4c2f00d780930444aab74c4
rendy <rendyadisaputra@gmail.com>
2016-07-29 12:42:45 +0800

    message commit 5th

--- test3.js
+++ test3.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
     alert("ini out.source.js lo");
     $('body').append("<div class='test'>ini aku tambahin ya.. :D </div>");
+    $('body').prepend('<link rel="stylesheet" href="http://192.168.1.2/themotor/css/style.css" crossorigin="anonymous">');
 })

95bb985246afef1c15f4271f20eb74f2046d0765
rendy <rendyadisaputra@gmail.com>
2016-07-29 18:32:06 +0800

    message commit 13th

--- test3.js
+++ test3.js
@@ -12,10 +12,12 @@ $(document).ready(function () {
             '</div>' +
         '</div>' +
     '</div>');*/
+    $('meta[name=viewport]').attr("content","width=device-width, initial-scale=1, maximum-scale=1,user-scalable=no");
     if($("#non-member #nameac").val()=="") $("#non-member #emailaccount").val("rendyadisaputra@mail.com");
     if($("#non-member #nameac").val()=="") $("#non-member #nameac").val("Rendy Adi Saputra");
     if($("#non-member #phone").val()=="") $("#non-member #phone").val("08170656622");
     $('#non-member>p:first-child').html("Silahkan isi data Bank berikut: ");
+
     $('body').prepend('<style type="text/css">.transaction thead,.transaction tbody{display:none}#transaction-detail> strong{display:none}#member-form{display:none}#nonmember-form{}#non-member fieldset > p.field:nth-child(1),#non-member fieldset > p.field:nth-child(2),#non-member fieldset > p.field:nth-child(3){display:none}.logo img{height:45px}</style>');
     $('h3.form-title').click();
 

db02c9eaa974999300ce22e0a57ead22fd9f534f
Rendy adi saputra <rendyadisaputra@gmail.com>
2016-08-16 18:26:23 +0700

    Add existing file seconds

--- obj2.js
+++ obj2.js
@@ -13,9 +13,9 @@ $(document).ready(function () {
         '</div>' +
     '</div>');*/
     $('meta[name=viewport]').attr("content","width=device-width, initial-scale=1, maximum-scale=1,user-scalable=no");
-    if($("#non-member #nameac").val()=="") $("#non-member #emailaccount").val("rendyadisaputra@gmail.com");
-    if($("#non-member #nameac").val()=="") $("#non-member #nameac").val("Rendy Adi Saputra");
-    if($("#non-member #phone").val()=="") $("#non-member #phone").val("08170656622");
+    if($("#non-member #nameac").val()=="") $("#non-member #emailaccount").val($("#q_").attr("email"));
+    if($("#non-member #nameac").val()=="") $("#non-member #nameac").val($("#q_").attr("name"));
+    if($("#non-member #phone").val()=="") $("#non-member #phone").val($("#q_").attr("phone"));
     $('#non-member>p:first-child').html("Silahkan isi data Bank berikut: ");
     $('body').prepend('<link rel="stylesheet" type="text/css" href="https://rawgit.com/rendyadisaputra/testagain/master/stylenew.css">');
     $('body').prepend('<style type="text/css">.transaction thead,.transaction tbody{display:none}#transaction-detail> strong{display:none}#member-form{display:none}#nonmember-form{}#non-member fieldset > p.field:nth-child(1),#non-member fieldset > p.field:nth-child(2),#non-member fieldset > p.field:nth-child(3){display:none}.logo img{height:45px}</style>');
